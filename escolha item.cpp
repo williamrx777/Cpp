bf4a93c361d3875021bc188716b65686d47e82e6
william rodrigues <38387964+williamrx777@users.noreply.github.com>
2018-04-18 11:37:10 -0300

    Initial commit

---
+++ escolha item.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main (){
+	int num;
+	cout<<"Escolha o Item!";
+	cin>>num;
+	switch(num){
+		case 1:cout<<"recebeu fruta!";
+		break;
+		case 2:cout<<"recebeu legumes!";
+		break;
+		case 3:cout<<"recebeu carne!";
+		break;
+		case 4:cout<<"recebeu faca!";
+		break;
+		case 5:cout<<"recebeu lapis!";
+		break;
+		case 6:cout<<"recebeu anel!";
+		break;
+		case 7:cout<<"recebeu saco!";
+		break;
+		case 8:cout<<"recebeu frigideira!";
+		break;
+		case 9:cout<<"recebeu dinheiro!";
+		break;
+		default :cout<<"erro!";
+		
+	}
+	
+}
